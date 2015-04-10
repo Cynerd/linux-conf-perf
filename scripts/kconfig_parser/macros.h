5a47ee52b95734ea4224adfd341ee0f9eb5c8d62
Karel Kočí <cynerd@email.cz>
2015-04-10 12:48:27 +0200

    Fix error printf

--- scripts/kconfig_parser/macros.h
+++ scripts/kconfig_parser/macros.h
@@ -1,6 +1,6 @@
 extern int verbose_level; // Defined in kconfig_parser.c
 
-#define Eprintf(...) printf(stderr, __VA_ARGS__)
+#define Eprintf(...) fprintf(stderr, __VA_ARGS__)
 #define Wprintf(...) if (verbose_level > 1) printf(__VA_ARGS__)
 #define Iprintf(...) if (verbose_level > 2) printf(__VA_ARGS__)
 
