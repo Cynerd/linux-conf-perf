2752b3c3802140163247348cba580e0320628941
Karel Kočí <cynerd@email.cz>
2015-04-12 11:48:10 +0200

    parse_kconfig clean include in parse.c

--- scripts/parse_kconfig/parser.c
+++ scripts/parse_kconfig/parser.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
-#include <unistd.h>
 #include <locale.h>
 #include <stdbool.h>
+#include <libintl.h>
 #include <argp.h>
 #include "kconfig/lkc.h"
 #include "symlist.h"
