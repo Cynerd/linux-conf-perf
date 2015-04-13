f4f33d092f5360338c5488ad5dbb2ac65244f09a
Karel Kočí <cynerd@email.cz>
2015-04-13 13:24:57 +0200

    Change comment in shared/macros.h

--- scripts/shared/macros.h
+++ scripts/shared/macros.h
@@ -1,4 +1,4 @@
-extern int verbose_level; // Defined in kconfig_parser.c
+extern int verbose_level; // Must be defined in project
 
 #define Eprintf(...) fprintf(stderr, __VA_ARGS__)
 #define Wprintf(...) if (verbose_level > 1) printf(__VA_ARGS__)
