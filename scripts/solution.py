a0779a7b455cc489f3a12e91dc8c9d18e3bc69d8
Karel Kočí <cynerd@email.cz>
2015-04-17 09:55:53 +0200

    Remove usage of .config fragment file

--- scripts/solution.py
+++ scripts/solution.py
@@ -90,10 +90,6 @@ def apply():
 
 	# Write solution to .config file in linux source folder
 	with open(conf.linux_sources + '/.config', 'w') as f:
-		with open(conf.dot_config_fragment_file, 'r') as fconf:
-			for line in fconf:
-				f.write(line)
-
 		for txt in solut:
 			if txt[0] == '-':
 				nt = True
