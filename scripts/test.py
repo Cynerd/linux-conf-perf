cb6536f354827db4c24b50be07bf22a1f1486faa
Karel Kočí <cynerd@email.cz>
2015-04-27 21:36:06 +0200

    Finish implementation of test.test

--- scripts/test.py
+++ scripts/test.py
@@ -3,13 +3,33 @@ import os
 import sys
 
 from conf import conf
+from conf import sf
 import initialize
+import iteration
 import kernel
 import boot
 
 def test():
 	initialize.gen_requred() # Call this to check initial solution
+	iteration.reset() # Reset iteration
+	conf.kernel_make_output = True
 	kernel.make()
+	conf.boot_output = True
+	boot.boot()
+	print('------------------------------')
+	for nm in os.listdir(sf(conf.output_folder)):
+		if os.path.isfile(os.path.join(sf(conf.output_folder), nm)):
+			with open(os.path.join(sf(conf.output_folder), nm), 'r') as f:
+				print(nm + ':')
+				for line in f:
+					print(line, end='')
+			os.remove(os.path.join(sf(conf.output_folder), nm))
+	# To be sure also try remove other file
+	try:
+		os.remove(sf(conf.solved_file))
+	except OSError:
+		pass
+
 
 #################################################################################
 
