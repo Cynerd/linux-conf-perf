a34cd8759407dbcb5f80a5a5500bcca09206e278
Karel Kočí <cynerd@email.cz>
2015-04-27 21:34:45 +0200

    Change boot.gen_nbscripts to support relative conf paths

--- scripts/boot.py
+++ scripts/boot.py
@@ -5,17 +5,19 @@ import shutil
 
 import utils
 from conf import conf
+from conf import sf
 from exceptions import MissingFile
 
 def gen_nbscript():
 	try:
-		os.remove(conf.nbscript)
+		os.remove(sf(conf.nbscript))
 	except OSError:
 		pass
 
-	with open(conf.nbscript, 'w') as f:
-		f.write('load ' + conf.linux_image + ' console=ttyS0,115200\n')
-		f.write('load ' + conf.initram + '\n')
+	with open(sf(conf.nbscript), 'w') as f:
+		f.write('# generated novaboot script. Please don\'t edit.\n')
+		f.write('load ' + sf(conf.linux_image) + ' console=ttyS0,115200\n')
+		f.write('load ' + sf(conf.initram) + '\n')
 
 def boot():
 	if not os.path.isfile(conf.nbscript):
