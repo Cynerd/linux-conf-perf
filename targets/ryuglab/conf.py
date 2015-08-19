cdc75b8c0f2459a4efc39a65dd51252ee8ec7280
Karel Kočí <cynerd@email.cz>
2015-08-19 18:34:50 +0200

    Move boot and remove compile script
    Compile script is not required if we use GNUmakefile.
    Holding script folder only because of boot script makes no sense.

--- targets/ryuglab/conf.py
+++ targets/ryuglab/conf.py
@@ -4,8 +4,8 @@
 
 kernel_arch = 'powerpc'
 kernel_env = {'SRCARCH': kernel_arch, 'ARCH': kernel_arch, 'KERNELVERSION': kernel_arch}
-build_command = ['../scripts/compile']
-boot_command = ['targets/ryuglab/scripts/boot']
+build_command = ['make']
+boot_command = ['targets/ryuglab/boot/boot']
 parse_command = ['tests/cyclictest/parse']
 
 db_user = 'kocikare'
